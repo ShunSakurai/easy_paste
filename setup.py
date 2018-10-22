75aeee77732b2fa82951d8d504367d1cd510824e
Shun Sakurai <justmyway.yui@gmail.com>
2018-10-22 18:56:21 +0900

    2.0.4 Support HTML code change in GitHub Releases page

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dict_console = {
     'dest_base': 'Easy Paste',
     'icon_resources': [(1, './icons/easy_paste_icon.ico')],
     'script': 'easy_paste.py',
-    'version': '2.0.3'
+    'version': '2.0.4'
 }
 
 dict_options = {
